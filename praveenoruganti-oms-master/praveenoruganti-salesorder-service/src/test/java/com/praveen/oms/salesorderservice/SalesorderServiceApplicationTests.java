b0f066c29432a8af9b7cd799a4db28b226149535
praveen <praveen@praveen-PC>
2019-11-13 10:16:22 +0530

    updated

---
+++ praveenoruganti-oms-master/praveenoruganti-salesorder-service/src/test/java/com/praveen/oms/salesorderservice/SalesorderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.oms.salesorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesorderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
